8058de529ac2dd8c19426b330678180e090bf180
MasWay <70774862+Channel-Gratisan@users.noreply.github.com>
2022-06-07 04:24:50 +0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 clear
-echo -e "=================================================" | lolcat
-echo -e "#     Premium Auto Script By MasWayVPN          #" | lolcat
-echo -e "=================================================" | lolcat
-echo -e "# For Debian 10 64 bit                          #" | lolcat
-echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
-echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
-echo -e "# Build Up By MasWayVPN                         #" | lolcat
-echo -e "=================================================" | lolcat
-echo -e "# Thanks To                                     #" | lolcat
-echo -e "=================================================" | lolcat
-echo -e "# Allah SWT                                     #" | lolcat
-echo -e "# My Family                                     #" | lolcat
-echo -e "# LamVpn                                        #" | lolcat
-echo -e "# Horasss                                       #" | lolcat
-echo -e "=================================================" | lolcat
+echo -e "=================================================" 
+echo -e "#     Premium Auto Script By MasWayVPN          #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By MasWayVPN                         #"
+echo -e "================================================="
+echo -e "# Thanks To                                     #"
+echo -e "================================================="
+echo -e "# Allah SWT                                     #"
+echo -e "# My Family                                     #"
+echo -e "# LamVpn                                        #"
+echo -e "# Horasss                                       #"
+echo -e "================================================="
