b367a78a66255951667ac3e2533b1e2a09a35b14
MasWay <70774862+Channel-Gratisan@users.noreply.github.com>
2022-06-06 08:47:58 +0700

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#     Premium Auto Script By MasWayVPN          #" | lolcat
+echo -e "=================================================" | lolcat
+echo -e "# For Debian 10 64 bit                          #" | lolcat
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
+echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
+echo -e "# Build Up By MasWayVPN                         #" | lolcat
+echo -e "=================================================" | lolcat
+echo -e "# Thanks To                                     #" | lolcat
+echo -e "=================================================" | lolcat
+echo -e "# Allah SWT                                     #" | lolcat
+echo -e "# My Family                                     #" | lolcat
+echo -e "# LamVpn                                        #" | lolcat
+echo -e "# Horasss                                       #" | lolcat
+echo -e "=================================================" | lolcat
